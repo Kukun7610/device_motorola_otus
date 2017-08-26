b885faa9a032b8a9f08c8cf18e30c46f2d2ca889
Kukun Mishra <kukun7610@gmail.com>
2017-08-26 10:34:38 +0530

    Update and rename dosp_otus.mk to aosp_otus.mk

--- aosp_otus.mk
+++ aosp_otus.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/motorola/otus/full_otus.mk)
 
 # Inherit some common DOSP stuff.
-$(call inherit-product, vendor/dosp/common.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Build Type
-EXTENDED_BUILD_TYPE=OFFICIAL
+EXTENDED_BUILD_TYPE=EXPERIMENTAL
 
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := dosp_otus
+PRODUCT_NAME := aosp_otus
 PRODUCT_RELEASE_NAME := MOTO E 3G
