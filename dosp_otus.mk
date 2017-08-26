624fde7f1a72e617c239d2fd2e4ee39c9b6a52cb
Kukun Mishra <kukun7610@gmail.com>
2017-08-26 10:41:32 +0530

    Update Build type

--- dosp_otus.mk
+++ dosp_otus.mk
@@ -4,9 +4,9 @@ $(call inherit-product, device/motorola/otus/full_otus.mk)
 $(call inherit-product, vendor/dosp/common.mk)
 
 # Build Type
-EXTENDED_BUILD_TYPE=OFFICIAL
+EXTENDED_BUILD_TYPE=EXPERIMENTAL
 
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := dosp_otus
+PRODUCT_NAME := aosp_otus
 PRODUCT_RELEASE_NAME := MOTO E 3G
